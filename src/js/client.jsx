e1894c70d65350f33af358d161dd7ebca36bb141
laurentpd <laurent.poulin-dube@mail.mcgill.ca>
2018-03-20 20:47:41 -0400

    terms and conditions page created and populated

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -11,6 +11,7 @@ import Solutions from "./pages/Solutions.jsx";
 import CreateProblem from "./pages/CreateProblem.jsx";
 import MyProblems from "./pages/MyProblems.jsx";
 import CreateAccount from "./pages/CreateAccount.jsx";
+import TermsConditions from "./pages/TermsConditions.jsx"
 const app = document.getElementById("app");
 
 ReactDOM.render(
@@ -24,6 +25,7 @@ ReactDOM.render(
       <Route path="createProblem(/:testvalue)" name="createProblem" component={CreateProblem}/>
       <Route path="myProblems" name="myProblems" component={MyProblems}/>
       <Route path="createAccount(/:testvalue)" name="createAccount" component={CreateAccount}/>
+      <Route path="termsConditions" name="termsConditions" component={TermsConditions}/>
     </Route>
   </Router>,
   app
