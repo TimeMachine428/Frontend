6313cba238f2b6da2431f80d4117c4b736c06e90
Justin <junyuu@hotmail.com>
2018-03-22 11:10:30 -0400

    added content on resources page

--- src/js/pages/Resources.jsx
+++ src/js/pages/Resources.jsx
@@ -2,9 +2,23 @@ import React from "react";
 
 export default class Resources extends React.Component {
    render() {
+   	var styles={
+
+   	}
+
     return (
-    	<div>
-    		<h1>Resources</h1>
+    	<div class="container">
+    		<div class="row">
+    			<h1>Resources</h1>	
+    		</div>
+    		<div class="row">
+	    		<div class="col-4">
+	    	 		<a class="badge badge-light" href="http://www.learncpp.com/" target="_blank"> C++ </a>
+	    	 		<div class="preview">
+	    	 			<iframe src="http://www.learncpp.com/" width = "300px" height = "300px" scrolling="no"></iframe>
+	    	 		</div>
+	    		</div>	
+    		</div>
     	</div>
     );
   }
