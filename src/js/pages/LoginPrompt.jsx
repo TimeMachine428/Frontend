f17f8828131cdab1ac5a72459203c2a5035d2239
laurentpd <laurent.poulin-dube@mail.mcgill.ca>
2018-03-24 14:00:45 -0400

    login prompt page added

---
+++ src/js/pages/LoginPrompt.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class LoginPrompt extends React.Component {
+
+    render() {
+        return (
+            <h1>Please Login to Access this Page</h1>
+        );
+    }
+}
