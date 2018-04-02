245bb7e999266e1d5525e9a89a625e26b2605f94
edward-son <edward_son@live.com>
2018-04-02 18:12:25 -0400

    continue fills current solution

--- src/js/pages/Solutions.jsx
+++ src/js/pages/Solutions.jsx
@@ -119,11 +119,9 @@ export default class Solutions extends React.Component {
               <h1>{this.problem.name}</h1>
                 <p>{this.problem.description}</p>
                 <p>Enter solution in the box below</p>
-
-                {/*<button onClick={this.onFeedback1}>Success Demo</button>
-                <button onClick={this.onFeedback2}>Fail Demo</button>*/}
-                <Textarea style = {{width:900, height: 300}} onChange={this.handleChange}/>
+                <Textarea style = {{width:900, height: 300}} defaultValue={this.props.location.state.solution} onChange={this.handleChange}/>
                 <button onClick={this.saveProgress}>Save Progress</button>
+
                 <a className="btn btn-default" onClick={this.handleSubmit}>Submit</a>
                 {testCaseList}
             </div>
